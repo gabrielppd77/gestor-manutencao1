a3b24119586375360fa4d77869bbe535a3e7875a
gabrielppd77 <gabrielppd77@outlook.com>
2021-03-23 22:24:53 -0300

    Project created

---
+++ backend/src/main/java/com/project/gestormanutencao/GestorManutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.project.gestormanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorManutencaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/gestormanutencao/GestorManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestormanutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
