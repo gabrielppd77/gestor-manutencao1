cc79e1d9e2dee8b96b621edd4260292373208a54
gabrielppd77 <gabrielppd77@outlook.com>
2021-03-26 13:02:37 -0300

    Finish endpoints Veiculo

--- backend/src/main/java/com/project/gestormanutencao/controllers/VeiculoController.java
+++ backend/src/main/java/com/project/gestormanutencao/controllers/VeiculoController.java
@@ -1,15 +1,23 @@
 package com.project.gestormanutencao.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import com.project.gestormanutencao.dto.VeiculoDTO;
+import com.project.gestormanutencao.entities.Veiculo;
 import com.project.gestormanutencao.services.VeiculoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -26,5 +34,33 @@ public class VeiculoController {
 
         return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<VeiculoDTO> findById(@PathVariable Long id) {
+        Veiculo veiculo = service.findById(id);
+
+        return ResponseEntity.ok().body(new VeiculoDTO(veiculo));
+    }
     
+    @PostMapping
+    public ResponseEntity<VeiculoDTO> insert(@RequestBody VeiculoDTO dto){
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<VeiculoDTO> update(@PathVariable Long id, @RequestBody VeiculoDTO dto) {
+        VeiculoDTO newVeiculo = service.update(id, dto);
+        return ResponseEntity.ok().body(newVeiculo);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/project/gestormanutencao/services/VeiculoService.java
+++ backend/src/main/java/com/project/gestormanutencao/services/VeiculoService.java
@@ -1,6 +1,7 @@
 package com.project.gestormanutencao.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.project.gestormanutencao.dto.VeiculoDTO;
@@ -8,6 +9,7 @@ import com.project.gestormanutencao.entities.Veiculo;
 import com.project.gestormanutencao.repositories.VeiculoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,5 +22,44 @@ public class VeiculoService {
         List<Veiculo> list = repository.findAll(); //PEGANDO OS VEICULOS E COLOCANDO NA LISTA
 
         return list.stream().map(x -> new VeiculoDTO(x)).collect(Collectors.toList()); //EXPRESSAO LAMBDA PARA TRANSFORMA OS VEICULOS EM VEICULOSDTO
+    }
+
+    public Veiculo findById(Long id){
+        Optional<Veiculo> veiculo = repository.findById(id);
+
+        return veiculo.get();
+    }
+
+    public VeiculoDTO insert(VeiculoDTO dto) {
+        Veiculo veiculo = new Veiculo(null, dto.getPlaca(), dto.getKm(), dto.getModelo(), dto.getFabricacao(), dto.getAno(), dto.getChassi(), dto.getTipo());
+
+        veiculo = repository.save(veiculo);
+
+        return new VeiculoDTO(veiculo);
+    }
+
+    public VeiculoDTO update(Long id, VeiculoDTO dto) {
+        Veiculo veiculo = findById(id);
+
+        veiculo.setPlaca(dto.getPlaca());
+        veiculo.setKm(dto.getKm());
+        veiculo.setModelo(dto.getModelo());
+        veiculo.setFabricacao(dto.getFabricacao());
+        veiculo.setAno(dto.getAno());
+        veiculo.setChassi(dto.getChassi());
+        veiculo.setTipo(dto.getTipo());
+
+        repository.save(veiculo);
+
+        return new VeiculoDTO(veiculo);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Veiculo não pode ser deletado! Tem associações");
+        }
+        
     }
 }
